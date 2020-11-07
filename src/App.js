badd5a3f32a528c42bb4529a16d8d85d03a7bbe6
matifandy <matifandy@gmail.com>
2020-11-07 14:06:26 -0300

    frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Header";
+import SwipeButtons from "./SwipeButtons";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* TinderCards */}
-      {/* SwipeButtons */}
+      <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
